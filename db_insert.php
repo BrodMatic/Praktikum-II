33cc1fbfc7c0e0ae5a2fb618f8b71d7d245a2047
BrodMatic <matic.brod@student.um.si>
2018-06-13 11:40:55 +0200

    sdasdasdasd

--- db_insert.php
+++ db_insert.php
@@ -6,7 +6,14 @@ $dbpwd = "admin_password";
 $dbname = "praktikum";
 
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-$sql ="INSERT INTO uporabnik (username, email, password) VALUES ('Somebody', 'somebody@gmail.com', 'helvette');";
+//$sql ="INSERT INTO uporabnik (username, email, password) VALUES ('Somebody', 'somebody@gmail.com', 'helvette');";
+$sql ="CREATE TABLE Uporabnik(
+    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    username varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255)
+  
+);";
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
 } else {
