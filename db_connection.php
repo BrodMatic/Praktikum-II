e0beca13c2585ff1d74c39b9816fa2238ae81ac3
BrodMatic <matic.brod@student.um.si>
2018-06-13 02:34:50 +0200

    Commit changed db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,3 +1,8 @@
  <?php
- $connection = mysqli_connect("localhost", "root", "", "praktikum") OR die ('Povezava do podatkovne baze ni uspela: ' . mysqli_connect_error() );
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = "admin_user";
+$dbpwd = "admin_password";
+$dbname = "praktikum";
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname) OR die ('Povezava do podatkovne baze ni uspela: ' . mysqli_connect_error() );
 ?> 
